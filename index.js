f03faaae13565ec36a4b66f6f1e3b00db880dd99
Anik <aniksarker1612@gmail.com>
2021-02-26 14:45:12 +0600

    path core module

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
-// dependencies 
+// dependencies
 const http = require('http');
+const url = require('url');
+const { StringDecoder } = require('string_decoder');
 
 // app object - module scaffolding
 const app = {};
 
 // configuration
 app.config = {
-    port: 5000,
+  port: 5000,
 };
 
 // create server
 app.createServer = () => {
-    const server = http.createServer(app.handleReqRes);
-    server.listen(app.config.port, () => {
-        console.log(`app listening to port ${app.config.port}`);
-    })
-}
+  const server = http.createServer(app.handleReqRes);
+  server.listen(app.config.port, () => {
+    console.log(`app listening to port ${app.config.port}`);
+  });
+};
 
 // handle Request Response
 app.handleReqRes = (req, res) => {
-    res.end("Hi, from node js app")
-}
+  // request handling
+  // get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
+  const method = req.method.toLowerCase();
+  const queryStringObject = parsedUrl.query;
+  const headers = req.headers;
+
+  const decoder = new StringDecoder('utf-8');
+  let realData = '';
+
+  req.on('data', (buffer) => {
+    realData += decoder.write(buffer);
+  });
+
+  req.on('end', () => {
+    realData += decoder.end();
+    console.log(realData);
+  });
+
+  // response handle
+  res.end('Hi, from node js app');
+};
 
 app.createServer();
